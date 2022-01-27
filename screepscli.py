963f861282ebed26eb97fcd510100a68a72e4670
igorg <igorg@fb.com>
2022-01-27 14:48:48 -0800

    use POST endpoint to send console commands

--- screepscli.py
+++ screepscli.py
@@ -65,6 +65,17 @@ def sysout(message):
     if message.startswith('time'):
         return
 
+    if message.startswith('protocol'):
+        return
+
+    if message.startswith('package'):
+        return
+
+    if message.startswith('on_message'):
+        data = json.loads(message[10:])
+        print('Error message received', data)
+        return
+
     data = json.loads(message)
 
     if 'messages' in data[1]:
@@ -75,15 +86,28 @@ def sysout(message):
     print('on_message', message)
 
 class ReadStdin(threading.Thread):
-    def __init__(self, connection):
+    def __init__(self, token):
         threading.Thread.__init__(self)
-        self.connection = connection
         self.running = True
+        self.token = token
 
     def run(self):
         while self.running:
             line = input()
-            self.connection.console(line)
+            sendconsole(self.token, line)
+
+def signin(email, password):
+    url = 'https://screeps.com/api/auth/signin'
+    data = { "email": email, "password": password,}
+    response = requests.post(url, data = data)
+    return response.json()['token']
+
+def sendconsole(token, expression):
+    url = 'https://screeps.com/api/user/console'
+    data = { "expression": expression, "shard": "shard3", "_token": token}
+    response = requests.post(url, data = data)
+    #  print ('response from sendconsole', response)
+
 
 def main():
     logging.basicConfig()
@@ -96,8 +120,10 @@ def main():
         sys.exit('Please set email and password as environment variables.')
 
     if arguments.get('log'):
+        token = signin(email, password)
+
         connection = Connection(email, password, arguments['--ptr'])
-        read_stdin = ReadStdin(connection)
+        read_stdin = ReadStdin(token)
         try:
             read_stdin.start()
             connection.startWebSocket(sysout)
